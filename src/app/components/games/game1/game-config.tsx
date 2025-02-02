1a0bc7edaf7b4009a0462b9e1a2bf395aba18f37
Devdat Kumar <Devdat_Kumar@Outlook.com>
2025-02-02 14:00:00 +0500

    Game configuration

---
+++ src/app/components/games/game1/game-config.tsx
@@ -0,0 +1,27 @@
+// game-config.tsx
+"use client";
+import { Types } from "phaser";
+import { Target } from "./scene1";
+
+const config: Types.Core.GameConfig = {
+  type: Phaser.CANVAS,
+  width: 1024,
+  height: 768,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        x: 0,
+        y: 0,
+      },
+      debug: true,
+    },
+  },
+  scene: [Target],
+};
+
+export default config;
