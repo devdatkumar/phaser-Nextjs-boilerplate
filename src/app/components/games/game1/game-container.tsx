c93e62988ad46df29010a62efe119aec09636df5
Devdat Kumar <Devdat_Kumar@Outlook.com>
2025-02-16 14:00:00 +0500

    current active scene removed

--- src/app/components/games/game1/game-container.tsx
+++ src/app/components/games/game1/game-container.tsx
@@ -2,29 +2,26 @@
 "use client";
 import { Game } from "phaser";
 import { useEffect, useRef } from "react";
-import { EventBus } from "./EventBus";
 import config from "./game-config";
 
 // Interface for ref forwarding
 export interface ContainerIRef {
   game: Phaser.Game | null;
-  scene: Phaser.Scene | null;
 }
 
 // Props interface
 interface ContainerIProps {
-  currentActiveScene?: (scene_instance: Phaser.Scene) => void;
   ref?: React.RefObject<ContainerIRef | null>;
 }
 
-export const GameContainer = ({ ref, currentActiveScene }: ContainerIProps) => {
+export const GameContainer = ({ ref }: ContainerIProps) => {
   const game = useRef<Phaser.Game | null>(null);
 
   useEffect(() => {
     if (!game.current) {
       game.current = new Game({ ...config });
       if (ref) {
-        ref.current = { game: game.current, scene: null };
+        ref.current = { game: game.current };
       }
     }
 
@@ -35,21 +32,6 @@ export const GameContainer = ({ ref, currentActiveScene }: ContainerIProps) => {
       }
     };
   }, [ref]);
-
-  useEffect(() => {
-    EventBus.on("current-scene-ready", (scene_instance: Phaser.Scene) => {
-      if (currentActiveScene) {
-        currentActiveScene(scene_instance);
-      }
-      if (ref) {
-        ref.current = { game: game.current, scene: scene_instance };
-      }
-    });
-
-    return () => {
-      EventBus.removeListener("current-scene-ready");
-    };
-  }, [currentActiveScene, ref]);
 
   return <div id="game-container" />;
 };
